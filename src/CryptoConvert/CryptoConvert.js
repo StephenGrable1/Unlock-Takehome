ca99676d90588a0de91b76c1a7091613ece5be33
StephenGrable1 <stephengrable@gmail.com>
2018-09-07 10:23:12 -0400

    build initial logic for conversion and create a dynamic input field; also edited styles and positioning of layout

--- src/CryptoConvert/CryptoConvert.js
+++ src/CryptoConvert/CryptoConvert.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import numeral from 'numeral';
 
 import './CryptoConvert.scss';
+import swap from './media/swap-icon.png'
 
 class CryptoConvert extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            inputValue: 1,
+            inputValue: 0,
             conversion: {
                 from: 'ETH',
                 to: 'USD'
@@ -17,7 +19,7 @@ class CryptoConvert extends React.Component {
                 USD: 0,
                 JPY: 0,
                 EUR: 0,
-                GDP: 0,
+                GBP: 0,
                 CAD: 0,
                 CHF: 0,
                 AUD: 0,
@@ -25,10 +27,13 @@ class CryptoConvert extends React.Component {
                 SEK: 0,
                 NZD: 0,
             }
-        }
+        };
         this.handleChange = this.handleChange.bind(this);
         this.handleSwitch = this.handleSwitch.bind(this);
-    }
+        this.convertSubUnits = this.convertSubUnits.bind(this);
+        this.selectFiat = this.selectFiat.bind(this);
+
+    };
 
     componentDidMount() {
         //fetching the current price of 1 ether in the top 10 most traded currencies
@@ -41,7 +46,7 @@ class CryptoConvert extends React.Component {
                         USD: data.USD,
                         JPY: data.JPY,
                         EUR: data.EUR,
-                        GDP: data.GDP,
+                        GBP: data.GBP,
                         CAD: data.CAD,
                         CHF: data.CHF,
                         AUD: data.AUD,
@@ -49,58 +54,132 @@ class CryptoConvert extends React.Component {
                         SEK: data.SEK,
                         NZD: data.NZD,
                     }
-                })
-            })
-    }
+                });
+            });
+    };
 
-    componentDidUpdate() {
-        console.log(this.state);
-    }
-
-    handleChange(e){
+    handleChange(e) {
         e.preventDefault();
-        this.setState({inputValue: e.target.value})
-    }
+        if (e.target.value < Math.pow(10, 17)) {
+            this.setState({ inputValue: e.target.value });
+        } else {
+            alert('Try choosing a smaller value');
+        };
+    };
 
-    handleSwitch(e){
+    handleSwitch(e) {
         e.preventDefault();
         const newTo = this.state.conversion.from;
         const newFrom = this.state.conversion.to;
 
-        this.setState({conversion: {
-            to: newTo,
-            from: newFrom
-        }})
-    }
+        this.setState({
+            conversion: {
+                to: newTo,
+                from: newFrom
+            }
+        });
+    };
 
-    
+    selectFiat(e) {
+        e.preventDefault();
+        const from = this.state.conversion.from;
+        const to = this.state.conversion.to;
+        this.setState({
+            conversion: {
+                from: from === 'ETH' ? from : e.target.value,
+                to: to === 'ETH' ? to : e.target.value
+            }
+        });
+    };
+
+    convertSubUnits(eth) {
+        let currAmount = eth.amount;
+        let displayAmount;
+        let displayUnit;
+
+        const subUnitOrder = ['wei', 'kwei', 'mwei', 'gwei', 'szabo', 'finney', 'ether', 'kether', 'mether', 'gether', 'tether'];
+        const subUnits = {
+            'wei': Math.pow(10, 18),
+            'kwei': Math.pow(10, 15),
+            'mwei': Math.pow(10, 12),
+            'gwei': Math.pow(10, 9),
+            'szabo': Math.pow(10, 6),
+            'finney': Math.pow(10, 3),
+            'ether': 1,
+            'kether': Math.pow(10, -3),
+            'mether': Math.pow(10, -6),
+            'gether': Math.pow(10, -9),
+            'tether': Math.pow(10, -12)
+        };
+
+        for (var i = 0; i < subUnitOrder.length; i++) {
+            let unit = subUnitOrder[i];
+            let conversion = currAmount * subUnits[unit];
+            if (conversion < 1000 && conversion > 1) {
+                displayAmount = conversion;
+                displayUnit = unit;
+                return (<p>{numeral(displayAmount).format('0.000') + ' ' + displayUnit}</p>);
+            };
+        };
+    };
 
     render() {
         var renderConversion = () => {
             const from = this.state.conversion.from;
             const to = this.state.conversion.to;
             const amount = this.state.inputValue;
-            if (from === 'ETH'){
-                return (
-                    <div>{amount * this.state.currencies[to]}</div>
-                )
-            }
+            const newConversion = this.state.inputValue / this.state.currencies[this.state.conversion.from]
 
-            if (from !== 'ETH'){
+            if (from === 'ETH') {
                 return (
-                    <div>{amount / this.state.currencies[from]}</div>
-                )
+                    <div>{numeral(amount * this.state.currencies[to]).format('0,0.00') + ' ' + this.state.conversion.to}</div>
+                );
+            };
+
+            if (to === 'ETH') {
+                return (
+                    <div>
+                        {newConversion ? this.convertSubUnits({ amount: newConversion, unit: 'ether' }) : 0 + ' ether'}
+                    </div>
+                );
+            };
+        };
+
+        var renderFiatSelect = (type) => {
+            return (
+                <select onChange={this.selectFiat} value={this.state.conversion[type]}>
+                    {renderFiatOptions(type)}
+                </select>
+            );
+        };
+
+        var renderFiatOptions = (type) => {
+            let options = [];
+            console.log('running fiat options outside: ', this.state.currencies.length)
+
+            for (let key in this.state.currencies) {
+                console.log('running fiat options: ')
+                options.push(<option key={key} value={key}>{key}</option>)
             }
+            return (options);
         }
+
         return (
             <div>
-                <h3>This app is ready!</h3>
-                From: {this.state.conversion.from}
-                <input type="number" value={this.state.inputValue} onChange={this.handleChange} min="10" max="100" />
-                <p>To: {this.state.conversion.to}</p>
-                <p>{this.state.etherUSD} </p>
-                {renderConversion()}
-                <div onClick={this.handleSwitch}>Switch</div>
+                <div className="switch-button" onClick={this.handleSwitch}><img src={swap}/></div>
+
+                <div className="conversion-container">
+                    <div className="from-container">
+                        <div className="from-title">From: {this.state.conversion.from !== 'ETH' ? renderFiatSelect('from') : this.state.conversion.from}</div>
+                        <input type="number" value={this.state.inputValue} onChange={this.handleChange} />
+                    </div>
+
+                    <div className="to-container">
+                        <div className="to-title">To: {this.state.conversion.to !== 'ETH' ? renderFiatSelect('to') : this.state.conversion.to}</div>
+                        {renderConversion()}
+                    </div>
+                </div>
+
             </div>
         );
     }
